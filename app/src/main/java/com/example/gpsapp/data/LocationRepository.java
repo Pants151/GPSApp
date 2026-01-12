27912f40189c955a478765c5cb36f945d8705ea6
Jose Antonio V.N <pantsoriginal22@gmail.com>
2026-01-12 12:24:43 +0100

    Ajuste de comentarios

--- app/src/main/java/com/example/gpsapp/data/LocationRepository.java
+++ app/src/main/java/com/example/gpsapp/data/LocationRepository.java
@@ -6,8 +6,8 @@ import android.location.Location;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.Priority; // Importante
-import com.google.android.gms.tasks.CancellationTokenSource; // Importante
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.CancellationTokenSource;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class LocationRepository {
